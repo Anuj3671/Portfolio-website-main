6838febc8516860d3f2b2a7ddff1ebdb6446edf4
ANUJ <agupta3671@gmail.com>
2024-08-09 16:50:03 +0530

    commit1
    jkd

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,7 +6,7 @@ import project4 from "../assets/projects/calc.jpg";
 
 
 
-export const HERO_CONTENT = `I am a dedicated web developer with a strong foundation in both front-end and back-end technologies. With expertise in HTML, CSS, Bootstrap, and React.js, I craft visually appealing and responsive user interfaces. On the back-end, my knowledge of Java and MySQL enables me to build robust and efficient applications. I am passionate about leveraging my skills to create innovative solutions that enhance user experience and drive business success.`;
+export const HERO_CONTENT = `I am a dedicated web developer with a strong foundation in both front-end and back-end technologies. With expertise in HTML, CSS, Bootstrap, tailwind and React.js, I craft visually appealing and responsive user interfaces. On the back-end, my knowledge of Java and MySQL enables me to build robust and efficient applications. I am passionate about leveraging my skills to create innovative solutions that enhance user experience and drive business success.`;
 
 export const ABOUT_TEXT = `I am a dedicated coder with a passion for solving complex problems and building efficient solutions. With expertise in HTML, CSS, and Bootstrap, I create clean and responsive web designs that prioritize user experience. I have a strong command of React.js, enabling me to develop interactive and dynamic web applications. My proficiency in Java and MySQL allows me to design robust back-end systems, ensuring data integrity and application performance. Constantly learning and adapting to new challenges, I strive to write clean, maintainable code that drives innovative projects forward.`;
 
