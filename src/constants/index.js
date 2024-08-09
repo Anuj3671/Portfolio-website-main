b01cf1bcd10c71b5882277d60673072cfb22cb9c
ANUJ <agupta3671@gmail.com>
2024-08-09 16:48:34 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,48 @@
+import project1 from "../assets/projects/pngegg.png";
+import project2 from "../assets/projects/todolist.jpg";
+
+import project3 from "../assets/projects/project-3.png";
+import project4 from "../assets/projects/calc.jpg";
+
+
+
+export const HERO_CONTENT = `I am a dedicated web developer with a strong foundation in both front-end and back-end technologies. With expertise in HTML, CSS, Bootstrap, and React.js, I craft visually appealing and responsive user interfaces. On the back-end, my knowledge of Java and MySQL enables me to build robust and efficient applications. I am passionate about leveraging my skills to create innovative solutions that enhance user experience and drive business success.`;
+
+export const ABOUT_TEXT = `I am a dedicated coder with a passion for solving complex problems and building efficient solutions. With expertise in HTML, CSS, and Bootstrap, I create clean and responsive web designs that prioritize user experience. I have a strong command of React.js, enabling me to develop interactive and dynamic web applications. My proficiency in Java and MySQL allows me to design robust back-end systems, ensuring data integrity and application performance. Constantly learning and adapting to new challenges, I strive to write clean, maintainable code that drives innovative projects forward.`;
+
+export const PROJECTS = [
+  {
+    title: "Connect4game",
+    image: project1,
+    description:
+      "The timeless strategy game where players race to connect four discs in a row, column, or diagonal before their opponent. Simple, fun, and endlessly engaging!",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "To-Do List",
+    image: project2,
+    description:
+      "Organize your tasks effortlessly with our intuitive to-do list app. Keep track of your priorities and achieve your goals with ease.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Simple Calculator",
+    image: project4,
+    description:
+      "A simple calculator project that performs basic arithmetic operations: addition, subtraction, multiplication, and division. Perfect for quick calculations and learning purposes.",
+    technologies: ["HTML", "CSS", "React"],
+  },
+];
+
+export const CONTACT = {
+  address: "Ghaziabad, Uttar Pradesh, 201005",
+  phoneNo: "+91 8860517446",
+  email: "agupta367@gmail.com",
+};
